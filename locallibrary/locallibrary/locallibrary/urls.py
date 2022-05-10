3b21da260fa5e6f1d6b4168492ccac28faeae7c3
Romans0103 <102057532+Romans0103@users.noreply.github.com>
2022-05-10 21:44:05 +0300

    include() чтобы доставлять URL из каталога приложения

--- locallibrary/locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/locallibrary/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+# Используйте include() чтобы доставлять URL из каталога приложения
+urlpatterns += [
+    path('catalog/', include('catalog.urls'))
 ]
