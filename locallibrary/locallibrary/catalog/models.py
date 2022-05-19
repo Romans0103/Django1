e6cb043c0fb100835b5a02420b917ef3621ef4b3
Romans0103 <102057532+Romans0103@users.noreply.github.com>
2022-05-19 20:07:15 +0300

    Возвращает URL-адрес для доступа к определенному экземпляру Моего имени модели Строка для представления объекта MyModelName (на сайте администратора и т.д.

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -2,9 +2,27 @@ from django.db import models
 
 # Create your models here.
 class MyModelName(models.Model):
-''' A typical class defining a model, derived from the Model class.
-'''
+    '''
+    A typical class defining a model, derived from the Model class.
+    '''
 
 # Fields
 my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
 
+# Metadata
+
+class Meta:
+    ordering = ['-m my_field_name']
+
+#  Methods
+def get_absolute_url(self):
+    '''
+    Returns the url to accses a particular instance of MyModelName
+    '''
+    return reverse('model-detail-view', args=[str(self.id)])
+
+def __str__(self):
+    '''
+    String for representing the MyModelName object (in Admin site etc.)
+    '''
+    return self.field_name
