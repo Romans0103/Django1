f95a6cdbd56384f948dfab1bebac06fd9a3a86f6
Romans0103 <102057532+Romans0103@users.noreply.github.com>
2022-05-19 19:51:00 +0300

    Определение модели исоздание рабочего поля

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MyModelName(models.Model):
+''' A typical class defining a model, derived from the Model class.
+'''
+
+# Fields
+my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
+
