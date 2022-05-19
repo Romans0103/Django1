5c7620b6965e18fd6cc8fee67f65f2008a747c09
Romans0103 <102057532+Romans0103@users.noreply.github.com>
2022-05-19 21:54:03 +0300

    Создание модели книги

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -54,3 +54,31 @@ class Genre(models.Model):
         String for representing the Model object(in Admin site etc)
         '''
         return self.name
+
+class Book(models.Model):
+    '''
+    Model representing a book (but not a specific copy of a book).
+    '''
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NUL, null=True)
+    # Foreign Key used because book can only have one author, but authors can have multiple books./
+    # Внешний ключ используется, потому что у книги может быть только один автор, но у авторов может быть несколько книг
+    # Author as a string rather than object because it hasn't been declared yet in the file./
+    # Автор в виде строки, а не объекта, потому что он еще не был объявлен в файле
+    summary = models.CharField(max_length=1000, help_text='Enter a brief description of the books')
+    isbn = models.CharField('ISBN', max_length=13,help_text='13 Character <a'
+'href="https://www.isbn-international.org/content/what-jsbn">JSBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genry for this book")
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defind so we can specify the object above.
+    def __str__(self):
+        '''
+        String for representing Model object.
+        '''
+        return self.title
+
+    def get_absolute_url(self):
+        '''
+        Returns the url to access a particular books instance.
+        '''
+        return reverse('book-detail', args=[str(self.id)])
